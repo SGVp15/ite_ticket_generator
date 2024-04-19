db4873a8397aebaafce5930f52bafe3559e23dbc
SGVp15 <phosphorusp15@mail.ru>
2024-04-19 17:11:29 +0300

    main

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ from Questions.Question import create_new_ticket, create_names_tickets
 from utils.utils import create_folders
 
 
-def create_txt(ticket, name):
+def create_txt(questions, name):
     with open(file=name + '.txt', mode='w', encoding='utf-8') as f:
-        for q in ticket:
+        for q in questions:
             f.write(f"{q['id_question']}\t{q['right_answer']}\t{q['mix']}\t{q['category']}\n".capitalize())
 
     num = re.findall(r"\d+", name)
     # print(num[0])
     with open(file=name + '_to_excel.txt', mode='w', encoding='utf-8') as f:
         f.write(f'{num[0]}\t\n')
-        for q in ticket:
+        for q in questions:
             f.write(f"{q['right_answer']}\t{q['category']}\n".capitalize())
 
 
@@ -35,9 +35,9 @@ if __name__ == '__main__':
         tickets_names = create_names_tickets(exam, num=5)
 
         # set_to_json(obj=ticket, file_name=f'{name}')
-        # create_txt(ticket=ticket, name=f'{name}')
         for ticket_name in tickets_names:
             ticket = create_new_ticket(all_questions, 5)
+            create_txt(ticket=ticket, name=f'{ticket_name}')
             create_docx(questions=ticket, name=f'{ticket_name}')
 
     print('\n[  convert_docx_to_pdf  ]')
